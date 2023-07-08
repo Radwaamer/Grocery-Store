8d9fa7781579253cf1d7d278c0b0827711a3385d
radwa amer <rororado44@gmail.com>
2023-07-08 14:08:50 -0700

    add sass structure and add banner section

--- home/js/home.js
+++ home/js/home.js
@@ -1,5 +1,30 @@
-(()=>{
-    document.querySelector("header .icons .search").addEventListener("click",()=>{
+function searchForm(){
         document.querySelector("header form").classList.toggle("active");
-    })
-})();
+};
+
+let bannerIndex=0;
+let bannerArrow="next";
+function bannerScroll(){
+    const bannerContent= document.querySelectorAll(".home .banner .content");
+    bannerContent[bannerIndex].classList.remove("active");
+    if(bannerIndex==bannerContent.length-1 && bannerArrow=="next"){
+        bannerContent[bannerIndex=0].classList.add("active");
+    }
+    else if(bannerIndex==0 && bannerArrow=="prev"){
+        bannerContent[bannerIndex=bannerContent.length-1].classList.add("active");
+    }
+    else{
+        bannerArrow=="next"? 
+        bannerContent[++bannerIndex].classList.add("active") :
+        bannerContent[--bannerIndex].classList.add("active") ;
+    }
+};
+
+function prev(){
+    bannerArrow="prev"
+    bannerScroll();
+}
+function next(){
+    bannerArrow="next"
+    bannerScroll();
+}
